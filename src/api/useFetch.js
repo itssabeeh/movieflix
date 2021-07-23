a7dd1e7e4931a98b4ef72b4737662d28578b3214
layone <itssabeeh@gmail.com>
2021-07-23 14:52:40 +0530

    midway with landing page

---
+++ src/api/useFetch.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const useFetch = (url) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [isError, setIsError] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const shows = await axios.get(url);
+      setData(shows);
+      setIsLoading(false);
+    } catch {
+      setIsError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+
+  return { isLoading, data };
+};
---
+++ src/components/ShowList/ShowList.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useFetch } from '../../api/useFetch';
+import './ShowList.css';
+const url = 'https://api.tvmaze.com/shows';
+const ShowList = () => {
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [shows, setShows] = useState([]);
+  // const [isError, setIsError] = useState(false);
+
+  // const fetchShows = async () => {
+  //   try {
+  //     const showlist = await axios.get(url);
+  //     setShows(showlist);
+  //     setIsLoading(false);
+  //   } catch {
+  //     setIsError(true);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchShows();
+  // }, []);
+  const { isLoading, data: shows } = useFetch(url);
+  const showlists = shows.data;
+  console.log(showlists);
+  return (
+    <div className="body-container">
+      <h1 style={{ textAlign: 'center' }}>STREAMING NOW</h1>
+      <div className="card-container">
+        {showlists &&
+          showlists.map((show) => {
+            const { id, name, genres, rating, weight, image, premiered } = show;
+            return (
+              <div key={id} className="cards">
+                <img className="show-image" src={image.medium}></img>
+                <h4 style={{ margin: '.25rem' }}>{name}</h4>
+                <h5 style={{ margin: '0.25rem', color: 'rgb(143, 73, 131)' }}>
+                  {premiered}
+                </h5>
+                <div className="show-genres">
+                  {genres &&
+                    genres.map((genre) => <div className="genre">{genre}</div>)}
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default ShowList;
